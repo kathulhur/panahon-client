43d1b338e119e4c124b5763508f97d36f37f5d0b
JosephCrisostomo <joseph.crisostomo@dashlabs.ai>
2023-10-12 12:49:55 +0800

    Remove revalidate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,15 @@
 import { Providers } from "./providers";
 
-
-export const revalidate = 300
 export default function RootLayout({
     children,
-  }: {
+}: {
     children: React.ReactNode;
-  }) {
+}) {
     return (
-      <html lang="en">
-        <body>
-          <Providers>
-            {children}
-          </Providers>
-        </body>
-      </html>
+        <html lang="en">
+            <body>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
     );
-  }
+}
